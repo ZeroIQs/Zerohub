ad69a24a4b795145c5d5a8cf638786c3d14ddaf1
Developer <developer@example.com>
2026-08-29 02:56:07 +0300

    Add no-cache headers and timestamp cache buster to run.ps1

--- run.ps1
+++ run.ps1
@@ -1,4 +1,8 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$b = (New-Object System.Net.WebClient).DownloadData('https://raw.githubusercontent.com/ZeroIQs/Zerohub/main/ZeroHub-GUI.ps1')
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add('Cache-Control', 'no-cache')
+$wc.Headers.Add('Pragma', 'no-cache')
+$ts = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
+$b = $wc.DownloadData("https://raw.githubusercontent.com/ZeroIQs/Zerohub/main/ZeroHub-GUI.ps1?v=$ts")
 $s = [System.Text.Encoding]::UTF8.GetString($b).TrimStart([char]0xFEFF)
 Invoke-Expression $s
